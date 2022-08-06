4e276cd0bd37269083497a88333cd02d1ac1bb18
aiswaryasaravanan <aiswaryasaravanan1998@gmail.com>
2022-08-06 10:32:55 +0530

    Python: Process CSV

---
+++ process-csv/process_csv.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import pdb
+
+def read_csv(file_path):
+    data = pd.read_csv(file_path)
+    return data
+
+def reformat_csv(data):
+    name = data['first_name'] + ' ' + data['last_name']
+    data['name'] = name
+    data = data.drop('first_name', axis=1)
+    data = data.drop('last_name', axis=1)
+    return data
+
+def write_csv(data):
+    data.to_csv("process-csv/output_py.csv", index=False)
+
+def main():
+    csv_path = "process-csv/input.csv"
+    input = read_csv(csv_path)
+    output = reformat_csv(input)
+    write_csv(output)
+
+if __name__ == "__main__":
+    main()
